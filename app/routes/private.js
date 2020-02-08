b92acea26f87c9b358afaa12f6d4c8749b7c9d51
Hovhannes <boyakhchyan1010@gmail.com>
2020-02-08 19:54:15 +0400

    just updates

--- app/routes/private.js
+++ app/routes/private.js
@@ -1,18 +1,37 @@
 import Route from "@ember/routing/route";
 import { inject as service } from "@ember/service";
 import RealtimeRouteMixin from "emberfire/mixins/realtime-route";
+// import RSVP from "rsvp";
 
 export default Route.extend(RealtimeRouteMixin, {
   session: service(),
+  store: service(),
 
   beforeModel() {
     if (!this.get("session.isAuthenticated")) {
       this.transitionTo("sign-in");
     }
   },
-  // return this.get("store").query("user", { orderBy: "email" });
+
+  // model() {
+  //   return RSVP.hash({
+  //     messages: this.store.query("message", { orderBy: "title" }),
+  //     users: this.store.query("user", { orderBy: "email" })
+  //   });
+  // },
+
+  // model() {
+  //   return RSVP.Promise.all([
+  //     this.store.query("message", { orderBy: "title" }),
+  //     this.store.query("user", { osderBy: "email" })
+  //   ]);
+  // },
 
   model() {
-    return this.get("store").query("message", { orderBy: "title" });
+    return this.store.query("message", { orderBy: "title" });
+  },
+
+  setupController(controller, model) {
+    controller.setProperties(model);
   }
 });
